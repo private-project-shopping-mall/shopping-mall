a8e076cedbc938c0025d6374f28000398bfb1945
MeteorLee <meteorproject529@gmail.com>
2023-04-21 21:56:50 +0900

    Feat: Item Repository 생성 #6

---
+++ src/main/java/com/shop/repository/ItemRepository.java
@@ -0,0 +1,8 @@
+package com.shop.repository;
+
+import com.shop.domain.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemRepository extends JpaRepository<Item, Long> {
+
+}
