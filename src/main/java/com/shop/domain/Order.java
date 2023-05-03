09ea3dab16d795ad1827d8f554ca9965830e8374
MeteorLee <meteorproject529@gmail.com>
2023-05-03 19:25:03 +0900

    feat: 다대일/일대다 양방향 매핑 #28

---
+++ src/main/java/com/shop/domain/Order.java
@@ -0,0 +1,32 @@
+package com.shop.domain;
+
+import com.shop.domain.constant.OrderStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+public class Order {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "order_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    private LocalDateTime orderDate; // 주문일
+
+    @Enumerated
+    private OrderStatus orderStatus; // 주문 상태
+
+    private LocalDateTime regTime;
+    private LocalDateTime updateTime;
+}
